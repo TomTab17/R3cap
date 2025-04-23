043dc73640c7e7b1940f48b04a16a5b44a837fe4
Tommaso Tabacchini <tom.tabacchini@stud.uniroma3.it>
2025-04-23 15:37:16 +0200

    Uploading initial project files

---
+++ src/main/java/it/uniroma3/siw/R3cap/R3capApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.R3cap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R3capApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R3capApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/R3cap/R3capApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.R3cap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R3capApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
